52072ed7eb55dcb6946403e5aa4933f8c527eef9
chenglai <137234911+chenglaiespino@users.noreply.github.com>
2023-12-02 11:32:12 +0800

    Add files via upload

---
+++ website/js/script.js
@@ -0,0 +1,29 @@
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');;
+    searchForm.classList.remove('active');
+    up.classList.remove('active');
+}
+
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+    navbar.classList.remove('active');
+    up.classList.remove('active');
+}
+
+let up = document.querySelector('.profile');
+
+document.querySelector('#user-btn').onclick = () =>{
+    up.classList.toggle('active');
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+}
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+    up.classList.remove('active');
+}
